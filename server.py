86b5adfa46bb998b90957e6c3ef73e8d4e37e58f
sanguinius121 <tuancoiz4@gmail.com>
2024-07-10 15:59:53 +0700

    remove the commends

--- server.py
+++ server.py
@@ -47,23 +47,6 @@ def handle_client(conn, addr):
             continue #skipping the header and continue
     conn.close()
 
-# def handle_client(conn, addr):
-#     print(f"[NEW CONNECTION: ] {addr} connected.")
-#     connected = True
-#     while connected:
-#         msg_length = conn.recv(HEADER).decode(FORMAT)
-#         if msg_length:
-#             print(msg_length)
-#             msg_length = int(msg_length)
-#             msg = conn.recv(msg_length)
-#             msg_decode = pickle.loads(msg)
-#             if msg_decode == DISCONNECT_MESSAGE:
-#                 connected = False
-#             #print(f"[{addr}:] {msg_decode}")
-#             with file_lock:
-#                 with open('server_data.txt', 'a') as file:
-#                     file.write(msg_decode + '\n')
-#     conn.close()
 
 def start():
     server.listen()
